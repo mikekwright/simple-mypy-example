38d404e4bd5f5222f69f3fca641462c6f781d60d
Michael Wright <mkwright@gmail.com>
2019-07-10 16:06:58 -0600

    Added the last part of the 2nd experiment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import random
 
+from typing import Callable, List
+
+
 def first(x: str) -> str:
     return f'first {x}'
 
@@ -7,5 +10,6 @@ def second(y: int) -> str:
     return f'second {y}'
 
 if __name__ == '__main__':
-    x = first if random.random() > 0.5 else second
+    funcs = [first, second]  # type: List[Callable[..., str]]
+    x = funcs[0] if random.random() > 0.5 else funcs[1] # type: Callable[[str], str]
     print(x('hi'))
